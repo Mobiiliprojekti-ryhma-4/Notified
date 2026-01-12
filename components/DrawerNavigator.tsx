61c47d1ed440e13e669c948765b1285dfb7d1e97
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-01-12 16:41:43 +0200

    edited more screens

--- components/DrawerNavigator.tsx
+++ components/DrawerNavigator.tsx
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import HomeScreen from '../screens/HomeScreen'
 import SecondScreen from '../screens/SecondScreen'
+import ThirdScreen from '../screens/ThirdScreen'
 
 const Drawer = createDrawerNavigator()
 
@@ -9,6 +10,7 @@ export default function DrawerNavigator() {
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Second" component={SecondScreen} />
+    <Drawer.Screen name="Third" component={ThirdScreen} />
     </Drawer.Navigator>
   )
 }
--- screens/ThirdScreen.tsx
+++ screens/ThirdScreen.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
-export default function ThirdScreen() {
+
+export default function ThirdScreen({ navigation }) {
   return (
     <View>
       <Text>ThirdScreen</Text>
+      <Button title="Open menu" onPress={() => navigation.openDrawer()} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+})
