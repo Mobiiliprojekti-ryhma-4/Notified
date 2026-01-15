5264104e35f047e7c17ac714419934132d639f9b
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-01-15 16:03:24 +0200

    env fixed

--- firebase/Config.ts
+++ firebase/Config.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrDSyPmclU3Sz0MfECJHzkbM6YXTui6Zk",
-  authDomain: "huoltohommeli.firebaseapp.com",
-  projectId: "huoltohommeli",
-  storageBucket: "huoltohommeli.firebasestorage.app",
-  messagingSenderId: "59928390205",
-  appId: "1:59928390205:web:c1e7069313a34840e51854"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig)
