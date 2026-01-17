7a5abc3fb7a56ba3c10a45d29310723e77027e5e
t3lami03 <t3lami03@students.oamk.fi>
2026-01-17 11:19:10 +0200

    Create colors.ts

---
+++ theme/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+    primary: '#579CD1', // pääväri, käytetään tärkeissä napeissa
+    secondary: '#A3B9A4', // toinen korostusväri
+    background: '#EADBC8', // taustaväri
+    text: '#21180D', // päätekstit
+    mutedText: '#4A3F35', // vähemmän tärkeät tekstit
+    specialColor: '#C9A96A' // ikonit, jatkoviivat, todella pienet yksityiskohdat 
+} as const;
+
+export default colors
