9954d607f7f8704670599ba567659713de76b869
IiroToivari <Iiro.Toivari@gmail.com>
2026-01-25 10:27:53 +0200

    Camera and time tracking V1

--- components/DrawerNavigator.tsx
+++ components/DrawerNavigator.tsx
@@ -2,6 +2,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import HomeScreen from '../screens/HomeScreen'
 import SecondScreen from '../screens/SecondScreen'
 import ThirdScreen from '../screens/ThirdScreen'
+import TimeTrackingScreen from '..//screens/TimeTrackingScreen'
+import PhotoScreen from '../screens/PhotoScreen'
 
 const Drawer = createDrawerNavigator()
 
@@ -11,6 +13,8 @@ export default function DrawerNavigator() {
       <Drawer.Screen name="HomeScreen" component={HomeScreen} />
       <Drawer.Screen name="SecondScreen" component={SecondScreen} />
     <Drawer.Screen name="ThirdScreen" component={ThirdScreen} />
+    <Drawer.Screen name="Työajanseuransa" component={TimeTrackingScreen} />
+     <Drawer.Screen name="Kuva" component={PhotoScreen} />
     </Drawer.Navigator>
   )
 }
